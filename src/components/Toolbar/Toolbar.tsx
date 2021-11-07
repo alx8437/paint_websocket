339946086b11dba5787e79a97ebd8ec21faae0ff
Aleksandr Zelenskiy <dezion777@gmail.com>
2021-11-07 22:08:03 +0300

    layout Toolbar.tsx

--- src/components/Toolbar/Toolbar.tsx
+++ src/components/Toolbar/Toolbar.tsx
@@ -4,8 +4,22 @@ import styles from './Toolbar.module.scss'
 const Toolbar = () => {
     return (
         <div className={styles.toolbar}>
-            <div className={styles.btn__container}></div>
-            <button className={`${styles.btn} ${styles.btn__brush}`}></button>
+            <div className={styles.btn__container}>
+                <div className={styles.btn_tools}>
+                    <button className={`${styles.btn} ${styles.btn__brush}`}/>
+                    <button className={`${styles.btn} ${styles.btn__rect}`}/>
+                    <button className={`${styles.btn} ${styles.btn__line}`}/>
+                    <button className={`${styles.btn} ${styles.btn__eraser}`}/>
+                    <button className={`${styles.btn} ${styles.btn__circle}`}/>
+                    <input style={{marginLeft: 5}} type="color"/>
+                </div>
+                <div className={styles.btn__nav}>
+                    <button className={`${styles.btn} ${styles.btn__undo}`}/>
+                    <button className={`${styles.btn} ${styles.btn__redo}`}/>
+                    <button className={`${styles.btn} ${styles.btn__save}`}/>
+                </div>
+            </div>
+
         </div>
     );
 };
