01c44bdbb56a6530d9b556e03d7075b98d7bae74
Aleksandr Zelenskiy <dezion777@gmail.com>
2022-02-01 19:04:04 +0300

    fix Line element foe canvas

--- src/tools/Line.ts
+++ src/tools/Line.ts
@@ -4,10 +4,13 @@ export default class Line extends Tool {
     mouseDown: boolean | null = null;
     beginX: number | null = null;
     beginY: number | null = null;
+    saved: string = '';
+
     constructor(canvas: HTMLCanvasElement | null) {
         super(canvas);
         this.listen()
     }
+
     listen() {
         this.canvas!.onmousemove = this.mouseMoveHandler.bind(this);
         this.canvas!.onmousedown = this.mouseDownHandler.bind(this);
@@ -16,27 +19,24 @@ export default class Line extends Tool {
 
     mouseUpHandler(event: any) {
         this.mouseDown = false;
-        console.log(event)
         if (this.beginX && this.beginX && event!.target!.offsetLeft) {
             this.draw(event.pageX - event!.target!.offsetLeft, event.pageY - event.target!.offsetTop)
         }
     }
 
     mouseDownHandler(event: any) {
-        console.log(event)
-        this.context!.beginPath()
         this.mouseDown = true;
-        // this.context!.beginPath();
         this.beginX = event.pageX - event.target.offsetLeft;
         this.beginY = event.pageY - event.target.offsetTop;
-        // this.context!.moveTo(
-        //     event.pageX - event.target.offsetLeft,
-        //     event.pageY - event.target.offsetTop
-        // )
+        this.context!.beginPath()
+        this.context!.moveTo(
+            this.beginX,
+            this.beginY,
+        )
+        this.saved = this.canvas!.toDataURL();
     }
 
     mouseMoveHandler(event: any) {
-        // console.log(event)
         if (this.mouseDown) {
             this.draw(
                 event.pageX - event.target.offsetLeft,
@@ -44,13 +44,20 @@ export default class Line extends Tool {
             );
         }
     }
-    //
-    draw(endX: number, endY: number) {
-        if (this.beginX && this.beginY) {
-            this.context!.moveTo(this.beginX, this.beginY);
-            this.context!.lineTo(endX, endY);
-            this.context!.stroke();
-        }
 
+    draw(endX: number, endY: number) {
+        const img = new Image()
+        img.src = this.saved
+        img.onload = () => {
+            if (this.beginX && this.beginY && this.canvas) {
+                this.context!.clearRect(0, 0, this.canvas!.width, this.canvas!.height)
+                this.context!.drawImage(img, 0, 0, this.canvas.width, this.canvas!.height)
+                this.context!.beginPath()
+                this.context!.moveTo(this.beginX!, this.beginY!)
+                this.context!.lineTo(endX, endY)
+                this.context!.stroke()
+            }
+
+        }
     }
 }
