b1fa3dde75c97f42c48d29058bb26fa5d7e4a4d8
aleksandr_zelenskiy <dezion777@gmail.com>
2022-04-21 17:37:14 +0400

    added react-router-dom

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,15 +2,24 @@ import Toolbar from "../Toolbar/Toolbar";
 import Settings from "../Settings/Settings";
 import Canvas from "../Canvas/Canvas";
 import styled from './App.module.scss';
+import { Routes, Route, Navigate } from "react-router-dom";
 
 
 function App() {
   return (
-    <div className={styled.app}>
-        <Toolbar />
-        <Settings />
-        <Canvas />
-    </div>
+          <div className={styled.app}>
+              <Routes>
+                  <Route path='/:id' element={
+                      <>
+                          <Toolbar/>
+                          <Settings/>
+                          <Canvas/>
+                      </>}>
+                  </Route>
+                  <Route path='*' element={<Navigate to={`f${(+new Date()).toString(16)}`} />}/>
+              </Routes>
+          </div>
+
   );
 }
 
